ac3e24975d4dac92c3abbc84dba99eb18ac7587d
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-06-21 22:14:58 +0100

    Added all files to work with

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("registration");
+    const feedbackDiv = document.getElementById("form-feedback");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        // Reference to all form inputs.
+        const userNameInput = document.getElementById("username");
+        const emailInput = document.getElementById("email");
+        const passwordInput = document.getElementById("password");
+
+        let isValid = true;
+        const messages = [];
+        
+        // Username validation
+        const trimUserName = userNameInput.Value.trim();
+        if (trimUserName.length < 3) {
+            isValid = false;
+            messages.push("Inavlid UserName Entered");
+        }
+
+        // Email Validation
+        const trimEmail = emailInput.Value.trim();
+        if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)) {
+            isValid = false;
+            messages.push("Inavlid Email Entered");
+        }
+
+        // Password Validation
+        if(passwordInput.length < 8) {
+            isValid = false;
+            messages.push("Password too short!!");
+        }
+
+        feedbackDiv.style.display = "block";
+        if (isValid) {
+            feedbackDiv.textContent = "Registration Successful";
+            feedbackDiv.style.color = "#28a745";
+        } else {
+            
+        }
+
+    })
+})
