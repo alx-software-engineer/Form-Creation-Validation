1d636ece85840b0ff4d0ff8b89dea057817bb1d7
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-06-22 12:18:46 +0100

    Create all files, and added codes

---
+++ fetch-data.js
@@ -0,0 +1,22 @@
+async function fetchUserData() {
+    const dataContainer = document.getElementById("api-data");
+    const response = "https://jsonplaceholder.typicode.com/users";
+    try {
+        const resolved = await fetch(response); 
+        const users = await resolved.json();
+        dataContainer.innerHTML = '';
+        const userList = document.createElement("ul");
+        users.forEach(element => {
+            const item = document.createElement("li");
+            item.textContent = element.name
+            userList.appendChild = item;
+        });
+
+        dataContainer.appendChild = userList;
+    } catch (error) {
+        dataContainer.innerHTML = '';
+        dataContainer.textContent = "Failed to load user data."
+    }
+}
+
+document.addEventListener("DOMContentLoaded", fetchUserData);
